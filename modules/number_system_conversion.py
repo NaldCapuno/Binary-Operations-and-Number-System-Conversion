0c8590663bec87687dca0a2c43b18366af656f78
NaldCapuno <jasoncapuno@gmail.com>
2024-02-03 18:57:54 +0800

    add binary to decimal function

--- modules/number_system_conversion.py
+++ modules/number_system_conversion.py
@@ -1,11 +1,35 @@
-def binary():
+def binary(x):
+    # Binary to Decimal
+    decimal = 0
+
+    if '.' in x:
+        whole = x[:x.index('.')]
+        frac = x[x.index('.')+1:]
+
+        for i in range(len(whole)-1, -1, -1):
+            decimal += int(whole[i]) * (2**(i))
+
+        for i in range(1, len(frac)+1):
+            decimal += int(frac[i-1]) * (2**(-i))
+
+    else:
+        for i in range(len(x)-1, -1, -1):
+            decimal += int(x[i]) * (2**(i))
+
+    # Binary to Hexa
+    hexa = 0
+            
+    # Binary to Octal
+    octal = 0
+    
+    print(f"\nDecimal: {decimal}\nHexa: {hexa}\nOctal: {octal}")
+    
+
+def decimal(x):
     pass
 
-def decimal():
+def octal(x):
     pass
 
-def octal():
-    pass
-
-def hexa():
+def hexa(x):
     pass
