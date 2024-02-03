55b921d3fe169b35fd21c151ad87cb10b7f1e7ee
NaldCapuno <jasoncapuno@gmail.com>
2024-02-03 22:05:39 +0800

    fix binary to decimal

--- modules/number_system_conversion.py
+++ modules/number_system_conversion.py
@@ -7,14 +7,14 @@ def binary(x):
         frac = x[x.index('.')+1:]
 
         for i in range(len(whole)-1, -1, -1):
-            decimal += int(whole[i]) * (2**(i))
+            decimal += int(whole[i]) * (2**(len(whole)-1-i))
 
         for i in range(1, len(frac)+1):
             decimal += int(frac[i-1]) * (2**(-i))
 
     else:
         for i in range(len(x)-1, -1, -1):
-            decimal += int(x[i]) * (2**(i))
+            decimal += int(x[i]) * (2**(len(x)-1-i))
 
     # Binary to Hexa
     hexa = 0
